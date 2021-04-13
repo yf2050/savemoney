56e392875c4a4e1acbd358ac9e146d17766e795c
vue <vue000@hotmail.com>
2021-04-14 00:59:35 +0800

    deploy

---
+++ precache-manifest.da9989158c7062468616e785f2d1b34b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0c1b646f50ba9fa7dd62",
+    "url": "/save-money/css/app.8bec634e.css"
+  },
+  {
+    "revision": "6d199e655c289bffca8c779fa54263ac",
+    "url": "/save-money/index.html"
+  },
+  {
+    "revision": "0c1b646f50ba9fa7dd62",
+    "url": "/save-money/js/app.8b2f37f6.js"
+  },
+  {
+    "revision": "59b9dbd143f3080e6883",
+    "url": "/save-money/js/chunk-vendors.2917b4a3.js"
+  },
+  {
+    "revision": "cde01b2da912ac285e61d9dde00acdf0",
+    "url": "/save-money/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/save-money/robots.txt"
+  }
+]);
